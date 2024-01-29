9effac732e04812012a4fb68e16df7257a2b401a
gokulkrish002 <itsgokul2002@gmail.com>
2024-01-29 19:47:22 +0530

    contact us section completed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let loop_runned = false;
 function reveal() {
     var elementVisible = 50;
     var windowHeight = window.innerHeight;
-    var reveals = document.querySelectorAll(".reveal-l,.reveal-r,.reveal-up-0_5,.reveal-up-1_0,.reveal-up-1_5,.reveal-up-2_0,.reveal-fade");
+    var reveals = document.querySelectorAll(".reveal-l,.reveal-r,.reveal-up-0_5,.reveal-up-1_0,.reveal-up-1_5,.reveal-up-2_0,.reveal-fade,.border-design-upside-down,.border-design");
     for (var i = 0; i < reveals.length; i++) {
         var elementTop = reveals[i].getBoundingClientRect().top;
         if (elementTop < windowHeight - elementVisible) {
